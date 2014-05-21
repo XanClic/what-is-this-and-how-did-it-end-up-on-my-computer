56e01b97554a24f1f8a444cfbd029d7fc97a7871
Max Reitz <xanclic@gmail.com>
2014-05-21 04:19:51 +0200

    kd trees: Reorganized
    * Split kd tree structures into nodes and the anchor; the anchor holds
      the vector with all the point references and the nodes only hold their
      respective iterators
    + Add information about split dimension and split value to the kd tree
      nodes

--- kd_tree.cpp
+++ kd_tree.cpp
@@ -5,9 +5,13 @@
 
 
 // Who needs other dimensions anyway
-template<> void kd_tree<3>::dump(unsigned level_indentation, unsigned indentation) const
+template<> void kd_tree_node<3>::dump(unsigned level_indentation, unsigned indentation) const
 {
-    printf("%*s%p\n", indentation, "", this);
+    if (leaf()) {
+        printf("%*s%p\n", indentation, "", this);
+    } else {
+        printf("%*s%p (dim %i, split %f)\n", indentation, "", this, split_dim, split_val);
+    }
     indentation += level_indentation;
 
     if (leaf()) {
@@ -18,4 +22,10 @@ template<> void kd_tree<3>::dump(unsigned level_indentation, unsigned indentatio
         left_child ->dump(level_indentation, indentation);
         right_child->dump(level_indentation, indentation);
     }
+}
+
+
+template<> void kd_tree<3>::dump(unsigned level_indentation, unsigned indentation) const
+{
+    root_node->dump(level_indentation, indentation);
 }
